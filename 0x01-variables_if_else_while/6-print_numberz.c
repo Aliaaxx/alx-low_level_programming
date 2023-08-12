e54e0b576cb9d67f10221829bfcc94859a1c5169
Aliaa <aliaa.assem4@gmail.com>
2023-08-11 17:43:21 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**                                                                               
+ * main - Entry point                                                             
+ *                                                                                
+ * Return: Always 0 (Success)                                                     
+ */
+int main(void)
+{
+int num = 48;
+while (num < 58)
+{
+putchar(num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
