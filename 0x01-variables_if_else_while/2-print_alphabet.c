27d9daf7174a977777f4f3f237883608d12628e0
Aliaa <aliaa.assem4@gmail.com>
2023-08-11 17:04:38 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter = 97;
+while (letter <= 122)
+{
+putchar(letter);
+letter += 1;
+}
+return (0);
+}
