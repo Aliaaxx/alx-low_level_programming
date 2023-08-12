e629a4dba4bdea0863a1ecbc33ef0162e8208d72
Aliaa <aliaa.assem4@gmail.com>
2023-08-11 18:27:47 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-  char letter = 97;
-  char  num = 48;
-  while (num < 58)
-    {
-      putchar(num);
-      num++;
-    }
-  while (letter < 103)
-    {
-      putchar(letter);
-      letter++;
-    }
-  putchar('\n');
-  return (0);
+char letter = 97;
+char  num = 48;
+while (num < 58)
+{
+putchar(num);
+num++;
+}
+while (letter < 103)
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
 }
