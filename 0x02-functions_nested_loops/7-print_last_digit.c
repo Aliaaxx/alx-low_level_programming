43fb5e2cded6b889b7755673eb285e0d8d0be782
Aliaa <aliaa.assem4@gmail.com>
2023-08-15 19:07:47 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - is a function that prints the last digit of
+ * a number
+ *
+ *@n: the Argument of the function
+ *
+ * Return: return 0 (Success)
+*/
+
+int print_last_digit(int n)
+{
+int digit;
+digit = n % 10;  
+return (digit);
+}
