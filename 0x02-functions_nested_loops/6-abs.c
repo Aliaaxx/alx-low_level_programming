13c904fb472cab535da97a5ef4aeec7beb51b989
Aliaa <aliaa.assem4@gmail.com>
2023-08-15 19:00:58 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - is a function thats returns the absolute value of an integer
+ *
+ *@n: the Argument of the function
+ *
+ * Return: return the absolute value (Success)
+*/
+
+int _abs(int n)
+{
+  return (abs(n));
+}
