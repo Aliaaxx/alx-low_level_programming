a2e9ba96db139e8e49c59e4bfd9b93b566b5fc87
Aliaa <aliaa.assem4@gmail.com>
2023-08-15 17:23:21 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * print_alphabet is a function thats prints the alphabet,
+ * print_alphabet -  is a function thats prints the alphabet,
  * in lowercase, followed by a new line 
 */
 void print_alphabet(void)
