3de4fe2e032aee1531239629a2b6105d556e28c9
Aliaa <aliaa.assem4@gmail.com>
2023-08-15 18:38:11 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,18 +2,18 @@
 #include "main.h"
 
 /**
- * _islower - is a function thats checks if the charcter is
- * in lowercase.
+ * _isalpha - is a function thats checks if the charcter is
+ * lowecase or uppercase
  *
  *@c: the Argument of the function
  *
- * Return: return 1 if 'c' is lowercase
+ * Return: return 1 if 'c' is lowercase or uppercase
  * other return 0 (Success)
 */
 
 int _isalpha(int c)
 {
-if ((c > 96 && c < 123)||(c > 65 && c < 91))
+if ((c > 96 && c < 123) || (c > 65 && c < 91))
 {
 return (1);
 }
