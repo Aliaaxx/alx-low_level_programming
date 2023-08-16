ad0913e49361a0692f233e2d40e4fc3ff7b8aebb
Aliaa <aliaa.assem4@gmail.com>
2023-08-16 07:34:01 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,11 +17,11 @@ for (k = 48; k < 54; k++)
 {
 for (r = 48; r < 54; r++)
 {
-_putchar('0' + i);
-_putchar('0' + j);
+_putchar(i);
+_putchar(j);
 _putchar(':');
-_putchar('0' + k);
-_putchar('0' + r);
+_putchar(k);
+_putchar(r);
 _putchar('\n');
 }
 }
