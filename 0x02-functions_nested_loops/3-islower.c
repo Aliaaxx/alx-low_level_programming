6ab1e8959cd51c72ca0144fe09322a9fc8e697e8
Aliaa <aliaa.assem4@gmail.com>
2023-08-15 17:40:03 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - is a function thats prints the alphabet,
+ * in lowercase, followed by a new line
+*/
+int _islower(int c);
+{
+if (c > 96 && c < 123)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
