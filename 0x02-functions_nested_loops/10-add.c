5d857404583594e534ed972c9a1705a153c7e32b
Aliaa <aliaa.assem4@gmail.com>
2023-08-15 19:27:34 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - is a function that returns sum of two numbers
+ *
+ *@n1: the first Argument of the function
+ *@n2: the second Argument of the function
+ *
+ * Return: return the sum (Success)
+*/
+
+int add(int n1, int n2)
+{
+int result;
+result = n1 + n2;
+return (result);
+}
