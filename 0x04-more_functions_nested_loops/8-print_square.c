59ce5e6fdec068de69466c52188c272ab7a919ba
Aliaa <aliaa.assem4@gmail.com>
2023-08-17 11:29:40 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - is a function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ * 
+ * @size: the Argument of the function
+*/
+void print_square(int size)
+{
+int i, j;
+for(i = 0; i < s; i++)
+{
+for(j=0; j < s; j++)
+{
+putchar('#');
+}
+putchar('\n');
+}
+}
