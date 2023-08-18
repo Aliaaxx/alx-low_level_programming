9079e7d0065aff37c7ab314be4577e177248cd35
Aliaa <aliaa.assem4@gmail.com>
2023-08-18 13:49:58 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,31 @@ int main(void)
 int i;
 for (i = 1; i < 101; i++)
 {
-if ((i % 3 == 0) && (i % 5 != 0))
-printf("Fizz");
-else if ((i % 5 == 0) && (i % 3 != 0))
+if (i == 100)
+{
 printf("Buzz");
-else if ((i % 3 == 0) && (i % 5 == 0))
-printf("FizzBuzz");
-else if (i == 100)
 break;
+}
+else if ((i % 3 == 0) && (i % 5 != 0))
+{
+printf("Fizz");
+printf(" ");
+}
+else if ((i % 5 == 0) && (i % 3 != 0))
+{
+printf("Buzz");
+printf(" ");
+}
+else if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz");
+printf(" ");
+}
 else
+{
 printf("%d", i);
 printf(" ");
+}
 }
 printf("\n");
 return 0;
