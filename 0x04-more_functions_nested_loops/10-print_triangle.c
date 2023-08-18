670cc04a37141f84146365c338855885b3024065
Aliaa <aliaa.assem4@gmail.com>
2023-08-18 16:47:45 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - is a function that draw a triangle
+ *
+ * @size: the Argument of the function
+ */
+void print_triangle(int size)
+{
+int i, j, k, count = size;
+if (size <= 0)
+{
+putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+if (j == count)
+{
+for(k = count; k <= size; k++)
+{
+putchar('#');
+}
+count--;
+break;
+}
+else
+{
+putchar(' ');
+}
+}
+putchar('\n');
+}
+}
+}
