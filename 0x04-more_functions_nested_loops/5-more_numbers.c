16a6387283dede9cc6082e79e3e6bf4835f1f9a5
Aliaa <aliaa.assem4@gmail.com>
2023-08-17 17:15:31 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,17 @@ void more_numbers(void)
 {
 int k, i, j;
 char arr[] = {"1011121314"};
-for(k = 0; k < 10; k++)
+for (k = 0; k < 10; k++)
 {
 for (i = 48; i < 58; i++)
 {
-_putchar(i);
+putchar(i);
 if (i == 57)
 {
 for (j = 0; j < 10; j++)
-_putchar(arr[j]);
+putchar(arr[j]);
 }
 }
-_putchar('\n');
+putchar('\n');
 }
 }
