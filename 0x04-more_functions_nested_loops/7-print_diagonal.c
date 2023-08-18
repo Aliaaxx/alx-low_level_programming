0919f45d2a57c5ea27ba55a21881523186ff52ac
Aliaa <aliaa.assem4@gmail.com>
2023-08-17 19:04:34 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,14 +20,14 @@ for (i = 0; i <= n; i++)
 {
 for (j = 0; j < i; j++)
 {
-//if (j == i - 1)
-//{
+if (j == i - 1)
+{
 putchar(92);  
-//}    
-//else
-//{
-//putchar(' ');
-//}
+}    
+else
+{
+putchar(' ');
+}
 }
 putchar('\n');
 }
