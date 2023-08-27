932dfe435ca96c4760c625b56d2236540c3dae95
Aliaa <aliaa.assem4@gmail.com>
2023-08-27 11:11:53 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - is a function that takes a pointer to an int
+ * as parameter and updates the value it points to to 98.
+ *
+ * @*n: the Argument of the function
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
