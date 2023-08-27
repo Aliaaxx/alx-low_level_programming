dccbfce060e6bcce0b644381040cc3d9cab2ba23
Aliaa <aliaa.assem4@gmail.com>
2023-08-27 11:13:47 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - is a function that takes a pointer to an int
  * as parameter and updates the value it points to to 98.
  *
- * @*n: the Argument of the function
+ * @n: the Argument of the function
  *
  * Return: nothing
  */
