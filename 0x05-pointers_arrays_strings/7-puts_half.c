a51aa4ff3bbe39ba76c8b8c2fc62127621f67a47
Aliaa <aliaa.assem4@gmail.com>
2023-08-27 15:51:24 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - is a function that reverses a string.
+ *
+ * @str: the Argument of the function
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+int i, half, len = strlen(s);
+if (len % 2 == 0)
+{
+half = len(s) / 2;
+for (i = half; i < len; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
+else
+{
+half = (len(s) - 1) / 2;
+for (i = half; i < len; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');  
+}
+}
