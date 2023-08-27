be2b2f9a045342547beafe3974b697dfec785318
Aliaa <aliaa.assem4@gmail.com>
2023-08-27 12:06:39 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - is a function that reverses a string.
+ *
+ * @s: the Argument of the function
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+puts(s);
+int i;
+for (i = strlen(s) - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
