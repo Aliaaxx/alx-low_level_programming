a98ca1903499fdadfa8ffe8b54d2756d6846c112
Aliaa <aliaa.assem4@gmail.com>
2023-08-27 17:10:08 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand();
+return (0);
+}
