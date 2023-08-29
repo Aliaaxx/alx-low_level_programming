015c5c4a71e79d6d5ddd7ee79888ae5b52741989
Aliaa <aliaa.assem4@gmail.com>
2023-08-28 19:56:44 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, j = size - 1, d1 = size + 1, index_d1 = 0, index_d2 = 0 ;
+int i, j = size - 1, d1 = size + 1, index_d1 = 0, index_d2 = 0;
 int sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
@@ -23,5 +23,5 @@ sum1 += (*(a + index_d1));
 sum2 += *(a + index_d2);
 j += (size - 1);
 }
-printf("%d, %d\n", sum1, sum2);    
+printf("%d, %d\n", sum1, sum2);
 }
