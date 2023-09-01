4efaad744096373910c89dd2cd1f936b474bd7cd
Aliaa <aliaa.assem4@gmail.com>
2023-09-01 14:51:53 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - is a function that appends the src string to the dest
+ * string, overwriting the terminating null byte (\0) at the end of 
+ * dest, and then adds a terminating null byte.
+ *
+ * @dest: the first Argument of the function
+ * @src: the second Argument of the function
+ *
+ * Return: Return value: the pointer to dest (Suceess)
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
