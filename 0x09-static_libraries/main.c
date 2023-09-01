88f9f325f4fd3d871c44474cb2011a294e3465a3
Aliaa <aliaa.assem4@gmail.com>
2023-09-01 16:29:53 -0700

    0x09-static_libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"My goal was to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
