71a847000bdd047139283b1867ef98bdba85f9f5
Aliaa <aliaa.assem4@gmail.com>
2023-09-01 15:07:53 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - is a function that compares two strings.
+ *
+ * @s1: the first Argument of the function
+ * @s2: the second Argument of the function
+ *
+ * Return: Return value(Suceess)
+ */
+int _strcmp(char *s1, char *s2)
+{
+return strcmp(s1, s2);
+}
