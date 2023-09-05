b35547d3848afac889813717c226f3f79537d411
Aliaa <aliaa.assem4@gmail.com>
2023-09-05 16:38:18 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
+int i;
 if (size == 0)
 {
 return (0);
@@ -19,6 +20,16 @@ return (0);
 else
 {
 char *arr = (char *)malloc(size * sizeof(char));
+if (arr == NULL)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+}
 return (arr);
 }
 }
