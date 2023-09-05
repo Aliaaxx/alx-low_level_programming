0e72ea9d02cdb491681aa11a5822ea506ec16282
Aliaa <aliaa.assem4@gmail.com>
2023-09-05 16:30:28 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char.
+ * create_array - creates an array of chars, and initializes
+ * it with a specific char.
  *
  * @size: The First Argument of the Function
  * @c: The Second Argument of the Function
