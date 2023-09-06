95b3c3685578cd78b854decaf8c32e91c6f84ad0
Aliaa <aliaa.assem4@gmail.com>
2023-09-05 18:25:17 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,14 +11,19 @@
  */
 int **alloc_grid(int width, int height)
 {
-int *ptr;
 int i, j;
-for (i = 0; i < width; i++)
+if (width <= 0 || height <= 0)
 {
+return (NULL);
+}
+int **grid = (int **)malloc(height * sizeof(int *));
+for (i = 0; i < height; i++)
+{
+grid[i] = (int *)malloc(width * sizeof(int));
 for (j = 0; j < height; j++)
 {
-ptr[i][j] = 0;
+grid[i][j] = 0;
 }
 }
-return (ptr);
+return (grid);
 }
