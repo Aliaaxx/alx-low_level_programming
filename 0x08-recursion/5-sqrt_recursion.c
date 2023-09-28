879f4e2a308acb20ca4987a67039965a6bea1fd0
Aliaaxx <aliaa.assem4@gmail.com>
2023-09-28 11:15:27 -0700

    0x08-recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - is a function that returns
+ * the natural square root of a number.
+ *
+ * @n: the Argument of the function
+ *
+ * Return: returns the natural square root of a number.
+ */
+int _sqrt_recursion(int n)
+{
+return (sqrt(n));
+}
