4ac3734b46ace383ac83afe059b220a6cddd3940
Aliaaxx <aliaa.assem4@gmail.com>
2023-10-02 16:24:54 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * @argc: The First Argument of the Function
+ * @argv: The Second Argument of the Function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * @argc: The First Argument of the Function
+ * @argv: The Second Argument of the Function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
