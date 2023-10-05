9fbed168b14c4180b2a387d44a53ec00aff73054
Aliaaxx <aliaa.assem4@gmail.com>
2023-10-04 18:11:24 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,8 +28,23 @@ return (i);
 char *str_concat(char *s1, char *s2)
 {
 int i, j = 0;
-int len1 = _strlen(s1);
-int len2 = _strlen(s2);
+int len1, len2;
+if (s1 == NULL)
+{
+len1 = 0;
+}
+else
+{
+len1 = _strlen(s1);
+}
+if (s2 == NULL)
+{
+len2 = 0; 
+}
+else
+{
+len2 = _strlen(s2);   
+}
 int size = len1 + len2;
 char *s = (char *)malloc((size + 1) * sizeof(char));
 if (s == NULL)
